eb5b51cd2919a1a1af6be7fc8f496af0bca124c1
João Lucas Gomes Câmara <202511900036@ifto.local>
2026-05-19 17:07:18 -0300

    Subindo arquivos do exemplo carro.py POO

--- POO/carro.py
+++ POO/carro.py
@@ -1,29 +1,87 @@
 # A palavra "class" é usada para criar uma classe.
 # Uma classe funciona como um molde para criar objetos.
 class Carro:
-    # "def" definir uma função ou método
-    # "__init__" é o método construtor da classe
-    # Ele é executado automaticamente quando um
-    # objeto é criado
+# "def" definir uma função ou método
+# "__init__" é o método construtor da classe
+# Ele é executado automaticamente quando um
+# objeto é criado
 
-    # "self" representa o próprio objeto
-    # É através do self que acessamos atributos e
-    # métodos do objeto
+# "self" representa o próprio objeto
+# É através do self que acessamos atributos e
+# métodos do objeto
 
-    # "marca", "modelo", "ano" e "velocidade"
-    # São Parâmetros recebidos pela classe
-     def __init__(self, marca, modelo, ano, velocidade = 0)
+# Método Construtor
+    def __init__(self, marca, modelo, ano, velocidade=0):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
         self.velocidade = velocidade
-    #Criando um objeto da Classe Carro
+        # Método
 
-        # "carro1" é uma variável que recebe um objeto
-        carro = Carro("Chevrolet", "S10", 2013)
+    def acelerar(self, aumento):
+        #self.velocidade = self.velocidade + aumento
+        self.velocidade += aumento
 
-        #Exibe informações do carro
-        print(f"Marca: {self.marca}")
-        print    
+        print(f"O carro acelerou para{self.velocidade} km/h")
+
+#Criando um objeto da Classe Carro
+
+# "carro1" é uma variável que recebe um objeto
+carro1 = Carro("Chevrolet", "S10", 2013, 0)
+
+#Exibe informações do carro
+print(f"Marca: {carro1.marca}")
+print(f"Modelo : {carro1.modelo}")
+print(f"Ano : {carro1.ano}")
+
+carro1.acelerar(50)
+
+carro2 = Carro("Ford", "Mobiauto", 2015, 0)
+
+#Exibe informações do carro
+print(f"Marca: {carro2.marca}")
+print(f"Modelo : {carro2.modelo}")
+print(f"Ano : {carro2.ano}")
+
+carro3 = Carro("Fiat", "Pulse Hybrid", 2024, 0)
+
+#Exibe informações do carroh
+print(f"Marca: {carro3.marca}")
+print(f"Modelo : {carro3.modelo}")
+print(f"Ano : {carro3.ano}")
+
+class Moto:
+
+    def __init__(self, marca, modelo, ano, velocidade=0):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.velocidade = velocidade
+
+moto1 = Moto("Suzuki", "Espirit", 2013, 0)
+
+
+print(f"Marca: {moto1.marca}")
+print(f"Modelo : {moto1.modelo}")
+print(f"Ano : {moto1.ano}")
+print(f"Velocidade : {moto1.velocidade} km/h")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
         
         
